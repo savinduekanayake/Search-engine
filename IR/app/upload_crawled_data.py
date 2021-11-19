bcceb3e97ef8c6bbad4a91fd3e50f0747ebe0d7f
Savindu Kalsara Ekanayake <47809365+savinduekanayake@users.noreply.github.com>
2021-11-19 06:48:45 +0530

    added the crawled data uploading file

--- IR/app/upload_crawled_data.py
+++ IR/app/upload_crawled_data.py
@@ -3,14 +3,13 @@ import json
 
 es = Elasticsearch([{'host': 'localhost', 'port':9200}])
 
-def data_upload():
+def upload_crawled_data():
     with open('data/data_final.json', encoding="utf8") as f:
         data = json.loads(f.read())
     helpers.bulk(es, data, index='index-ministers')
     print(es.indices.get_mapping())
     # es.indices.delete(index='index-ministers', ignore=[400, 404])
-
-
+    
 
 if __name__ == "__main__":
-    data_upload()
+    upload_crawled_data()
